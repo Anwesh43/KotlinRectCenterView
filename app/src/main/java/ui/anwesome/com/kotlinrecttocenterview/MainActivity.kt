2657722217533adae843a48a9bef88a5be8ec9fe
Anwesh43 <anweshthecool0@gmail.com>
2018-03-20 09:07:00 +0530

    using view in activity

--- app/src/main/java/ui/anwesome/com/kotlinrecttocenterview/MainActivity.kt
+++ app/src/main/java/ui/anwesome/com/kotlinrecttocenterview/MainActivity.kt
@@ -2,11 +2,12 @@ package ui.anwesome.com.kotlinrecttocenterview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import ui.anwesome.com.recttocenterview.RectToCenterView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        RectToCenterView.create(this)
     }
 }
